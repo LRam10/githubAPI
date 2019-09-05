a1227bb47c92aa5cdd7427b8aea04cc6fe423770
LuisRamirez <Lramirez@na.edu>
2019-09-05 17:38:16 -0500

    homepage not showing

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -1,8 +1,8 @@
-// if("serviceWorker" in navigator){
-//     try {
-//         navigator.serviceWorker.register('sw.js');
-//         console.log('register')
-//     } catch (error) {
-//         console.log("error");
-//     }
-// }
+ if("serviceWorker" in navigator){
+     try {
+        navigator.serviceWorker.register('sw.js');
+         console.log('register')
+     } catch (error) {
+         console.log("error");
+     }
+ }
