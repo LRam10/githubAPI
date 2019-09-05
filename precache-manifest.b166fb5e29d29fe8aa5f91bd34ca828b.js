47dab806752044822f17bfbc95b6b9ceeaa90317
LuisRamirez <Lramirez@na.edu>
2019-09-05 17:24:27 -0500

    Updates

---
+++ precache-manifest.b166fb5e29d29fe8aa5f91bd34ca828b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a30512af4169ac821213f5d892ef06bf",
+    "url": "/githubAPI/index.html"
+  },
+  {
+    "revision": "2275d8b4c7bfd602a6b1",
+    "url": "/githubAPI/static/css/main.b8769e04.chunk.css"
+  },
+  {
+    "revision": "732acdf0b59ffb7f5a1f",
+    "url": "/githubAPI/static/js/2.73376815.chunk.js"
+  },
+  {
+    "revision": "2275d8b4c7bfd602a6b1",
+    "url": "/githubAPI/static/js/main.2b91f242.chunk.js"
+  },
+  {
+    "revision": "3ef58856f3d5a2b547b8",
+    "url": "/githubAPI/static/js/runtime~main.9718a106.js"
+  }
+]);
