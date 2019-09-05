d9c51962c935627de1e00a60eedb597fdb87f4f5
LuisRamirez <Lramirez@na.edu>
2019-09-05 18:04:32 -0500

    Updates

---
+++ precache-manifest.3b037fe79d40903dc144c28bdbb0e54d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5715f011222b80b73939dbed83f2f12d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c518424b1512bdddf22a",
+    "url": "./static/css/main.b8769e04.chunk.css"
+  },
+  {
+    "revision": "732acdf0b59ffb7f5a1f",
+    "url": "./static/js/2.73376815.chunk.js"
+  },
+  {
+    "revision": "c518424b1512bdddf22a",
+    "url": "./static/js/main.be136519.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
